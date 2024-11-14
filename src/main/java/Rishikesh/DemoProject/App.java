d372d1b3bebe94439fc0753031929d538fed33e6
Rishikesh-Reddy-Ch <cherukurishi95@gmail.com>
2024-11-14 15:22:28 +0530

    initialization

---
+++ src/main/java/Rishikesh/DemoProject/App.java
@@ -0,0 +1,10 @@
+package Rishikesh.DemoProject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Rishikesh-22BD1A0507");
+    }
+}
